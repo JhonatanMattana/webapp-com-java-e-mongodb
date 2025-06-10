1178bb28fce2df06c40b223e3292166be185aef2
JhonatanMattana <kaverinhaa@hotmail.com>
2025-06-09 22:19:05 -0300

    acessando mongo e recuperando dado com metodo find()

--- src/main/java/br/com/mongo/executeapp/Principal.java
+++ src/main/java/br/com/mongo/executeapp/Principal.java
@@ -1,11 +1,21 @@
 package br.com.mongo.executeapp;
 
+import org.bson.Document;
+
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		MongoClient mongoClient = new MongoClient();
+		MongoClientURI uri = new MongoClientURI("mongodb://admin:admin@localhost:27017/?authSource=admin");
+		MongoClient client = new MongoClient(uri);
+		MongoDatabase database = client.getDatabase("test");
+		MongoCollection<Document> alunos = database.getCollection("alunos");
+		Document aluno = alunos.find().first();
+		System.out.println(aluno);
 	}
 
 }
